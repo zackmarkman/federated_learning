4d50bddb766298ac888e39b145bee3e61c1efc29
MartinChai1998 <martin.c1164@gmail.com>
2021-05-13 18:08:10 +1000

    progressss

--- COMP3221_FLClient.py
+++ COMP3221_FLClient.py
@@ -1,5 +1,6 @@
 import sys
 import socket
+import pickle
 
 import torch
 import torch.nn as nn
@@ -15,5 +16,93 @@ CLIENT_ID = sys.argv[1]
 PORT_CLIENT = int(sys.argv[2])
 OPT_FLAG = int(sys.argv[3])
 
+# Tunable parameters
+learning_rate = 0.01
+batch_size = 20
+
+
+
+def get_data(id=""):
+    train_path = os.path.join("FLdata", "train", "mnist_train_client" + str(id) + ".json")
+    test_path = os.path.join("FLdata", "test", "mnist_test_client" + str(id) + ".json")
+    train_data = {}
+    test_data = {}
+
+    with open(os.path.join(train_path), "r") as f_train:
+        train = json.load(f_train)
+        train_data.update(train['user_data'])
+    with open(os.path.join(test_path), "r") as f_test:
+        test = json.load(f_test)
+        test_data.update(test['user_data'])
+
+    X_train, y_train, X_test, y_test = train_data['0']['x'], train_data['0']['y'], test_data['0']['x'], test_data['0']['y']
+    X_train = torch.Tensor(X_train).view(-1, 1, 28, 28).type(torch.float32)
+    y_train = torch.Tensor(y_train).type(torch.int64)
+    X_test = torch.Tensor(X_test).view(-1, 1, 28, 28).type(torch.float32)
+    y_test = torch.Tensor(y_test).type(torch.int64)
+    train_samples, test_samples = len(y_train), len(y_test)
+    return X_train, y_train, X_test, y_test, train_samples, test_samples
+
+
+class MCLR(nn.Module):
+    def __init__(self):
+        super(MCLR, self).__init__()
+        self.fc1 = nn.Linear(784, 10)
+
+    def forward(self, x):
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+
 class Client():
-    pass
+    def __init__(self, client_id, model, learning_rate, batch_size):
+        self.X_train, self.y_train, self.X_test, self.y_test, self.train_samples, self.test_samples = get_data(
+            client_id)
+        self.train_data = [(x, y) for x, y in zip(self.X_train, self.y_train)]
+        self.test_data = [(x, y) for x, y in zip(self.X_test, self.y_test)]
+        self.trainloader = DataLoader(self.train_data, batch_size)
+        self.testloader = DataLoader(self.test_data, self.test_samples)
+        self.loss = nn.NLLLoss()
+        self.model = copy.deepcopy(model)
+        self.id = client_id
+        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=learning_rate)
+
+    def set_parameters(self, model):
+        for old_param, new_param in zip(self.model.parameters(), model.parameters()):
+            old_param.data = new_param.data.clone()
+
+    def train(self, epochs):
+        LOSS = 0
+        self.model.train()
+        for epoch in range(1, epochs + 1):
+            self.model.train()
+            for batch_idx, (X, y) in enumerate(self.trainloader):
+                self.optimizer.zero_grad()
+                output = self.model(X)
+                loss = self.loss(output, y)
+                loss.backward()
+                self.optimizer.step()
+        return loss.data
+
+    def test(self):
+        self.model.eval()
+        test_acc = 0
+        for x, y in self.testloader:
+            output = self.model(x)
+            test_acc += (torch.sum(torch.argmax(output, dim=1) == y) * 1. / y.shape[0]).item()
+            print(str(self.id) + ", Accuracy of client ", self.id, " is: ", test_acc)
+        return test_acc
+
+    def send_local_model(self):
+        return
+
+    def run(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((IP, PORT_SERVER))
+            message = (CLIENT_ID, self.train_samples)
+            mess = pickle.dumps(message)
+            s.sendall(mess)
+            while (True):
+                data_recv = s.recv(65536)
--- COMP3221_FLServer.py
+++ COMP3221_FLServer.py
@@ -1,5 +1,8 @@
 import sys
 import socket
+import pickle
+import time
+import threading
 
 import torch
 import torch.nn as nn
@@ -13,5 +16,64 @@ PORT_HOST = sys.argv[1]
 SUB_CLIENTS = int(sys.argv[2])
 IP = "127.0.0.1"
 
+
+class MCLR(nn.Module):
+    def __init__(self):
+        super(MCLR, self).__init__()
+        self.fc1 = nn.Linear(784, 10)
+
+    def forward(self, x):
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+
 class Server():
-    pass
+    def __init__(self, *args):
+        self.args = args
+        self.clients = set() # List of clients
+        self.num_iters = 100
+        self.loss = []
+        self.accuracy = []
+        self.global_train_size = 0
+        self.client_IDs = {}
+        self.num_connected = 0
+
+    def run(self):
+        # Create a new socket
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:  # created an object
+            print("Server start")
+            print("Server host names: ", IP, "Port: ", PORT_HOST)
+            s.bind((IP, PORT_HOST))
+            s.listen(5)
+
+            # Initiate model
+            global_model = MCLR()
+            t_start = time.time()
+
+            while time.time() - t_start < 30:
+                # Establish connection from client
+                client, addr = s.accept()
+                self.clients.add(client)
+                mess_recv = client.recv(65536) # clientID and train size
+                clientID, client_train_size = pickle.loads(mess_recv)
+                self.client_IDs[client] = clientID
+                self.global_train_size += client_train_size
+                self.num_connected = len(self.clients)
+
+            for i in range(self.num_iters):
+                print("Global iteration {}:".format(i))
+                print("Total number of clients:",self.num_connected)
+
+                global_model_file = open('global_model', 'w+b')
+                glob_send = pickle.dump(global_model)
+
+
+                for client in self.clients:
+                    s.send()
+
+
+
+
+
